d4b7a437d96b4f8a2b84cb5804198da6e3055672
thiagonmiziara <thiagonmiziara@gmail.com>
2024-02-13 16:54:44 -0300

    config: add sonner toast

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -20,9 +21,20 @@ export function SignIn() {
   } = useForm<signInForm>()
 
   async function handleSignIn(data: signInForm) {
-    console.log(data)
+    try {
+      console.log(data)
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para seu e-mail!', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch {
+      toast.error('E-mail inválido!')
+    }
   }
 
   return (
